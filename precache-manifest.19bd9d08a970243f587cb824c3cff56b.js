cd864664bbac1aff7eac64be15f4864f505e45f2
Sharath George M <sharathgeorgem@gmail.com>
2018-11-13 05:30:45 +0530

    Updates

---
+++ precache-manifest.19bd9d08a970243f587cb824c3cff56b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/spotifyx/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ee30580f41315316197",
+    "url": "/spotifyx/static/js/runtime~main.2ee30580.js"
+  },
+  {
+    "revision": "0ea297a5272db06a5f3d",
+    "url": "/spotifyx/static/js/main.0ea297a5.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/spotifyx/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "0ea297a5272db06a5f3d",
+    "url": "/spotifyx/static/css/main.59a3f8ab.chunk.css"
+  },
+  {
+    "revision": "3626b86fb521c255ec7bee05de418f7d",
+    "url": "/spotifyx/index.html"
+  }
+];
