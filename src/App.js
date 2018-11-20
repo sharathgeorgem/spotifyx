9917ae6f4decec64a282d78cc75323bd8a9785a6
Sharath George M <sharathgeorgem@gmail.com>
2018-11-20 08:51:48 +0530

    Added fetch using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import './App.css'
 // App
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      categories: []
+    }
+  }
+  componentDidMount () {
+    axios.get('https://cors-anywhere.herokuapp.com/https://beatsapi.media.jio.com/v2_1/beats-api/jio/src/response/home/english')
+      .then(res => res)
+      .then(response => {
+        console.log('The response is ', response)
+        let categories = response.data
+        this.setState({
+          categories
+        })
+        console.log('The categories are ', this.state.categories.result.data)
+      })
+  }
   render () {
     return (
       <div className='App'>
